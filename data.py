be1071e0e45ce2126fe4a2474c8c0b1abe392212
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-04-20 19:49:25 +0900

    dash intro

---
+++ data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+
+def make_condition_df(condition, country=None):
+    df = pd.read_csv(f"data/time_series_{condition}.csv")
+
+    if country:
+        df = df.loc[df["Country/Region"] == country]
+
+    df = df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum()
+    # df = df.iloc[:, :561]
+    df = df.reset_index(name=condition).rename(columns={"index": "date"})
+    return df
+
+
+def make_df(country=None):
+    final_df = None
+    for condition in conditions:
+        condition_df = make_condition_df(condition, country)
+
+        if final_df is None:
+            final_df = condition_df
+        else:
+            final_df = final_df.merge(condition_df)
+
+    return final_df
+
+
+daily_dataframe = pd.read_csv("data/daily_report_2021-03-06.csv")
+
+totals_df = daily_dataframe[["Confirmed", "Deaths", "Recovered"]].sum()
+totals_df = totals_df.reset_index(name="count").rename(columns={"index": "condition"})
+
+countries_df = daily_dataframe[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum()
+countries_df = countries_df.reset_index()
+
+conditions = ["confirmed", "deaths", "recovered"]
+
+global_df = make_df()
+print(totals_df)
+print(countries_df)
+print(global_df)
+print(make_df("Korea, South"))
