5b09b5224f5bc7da591e4792378bb03ba99224ea
rosenrose <crazytempler@gmail.com>
2022-04-21 13:12:35 +0900

    layouts

--- main.py
+++ main.py
@@ -2,7 +2,35 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
-app = Dash(__name__)
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
+]
+
+app = Dash(__name__, external_stylesheets=stylesheets)
+
+app.layout = html.Div(
+    style={
+        "minHeight": "100vh",
+        "color": "white",
+        "backgroundColor": "#333",
+        "fontFamily": "Open Sans, sans-serif",
+    },
+    children=[
+        html.Header(
+            style={"paddingTop": "3rem"},
+            children=[
+                html.H1(
+                    "COVID-19 Dashboard",
+                    style={
+                        "textAlign": "center",
+                        "fontSize": "3rem",
+                    },
+                )
+            ],
+        )
+    ],
+)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
