231fd2e6655e092437068f2c25b21fc41136fe36
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-04-25 12:17:58 +0900

    d

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
 from data import (
     daily_reports,
     countries_df,
